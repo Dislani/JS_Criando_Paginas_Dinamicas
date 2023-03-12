b40ffb24b2bf56174418ee826d866c9e977ad137
Dislani <dismelo16@gmail.com>
2023-03-11 23:37:29 -0300

    Lista de elementos e referências

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 function tocaSomPom () {
 
     document.querySelector('#som_tecla_pom').play();
+
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+listaDeTeclas[0].onclick = tocaSomPom;
 
-function tocaSomClap () {
-
-    document.querySelector('#som_tecla_clap').play();
-}
-
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
