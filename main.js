f637ce22c2247592f963a7963097a70ab4d934b6
Dislani <dismelo16@gmail.com>
2023-03-13 20:57:37 -0300

    Eventos e Lógicas

--- main.js
+++ main.js
@@ -1,28 +1,45 @@
-function tocaSom (idElementoAudio) {
+function tocaSom(selectorAudio) {
+    const elemento = document.querySelector(selectorAudio);
 
-    document.querySelector(idElementoAudio).play();
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
 
+    else {
+        console.log('Elemento não encontrado ou selector inválido');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+/* let contador = 0;
 
-let contador = 0;
-//enquanto
-while(contador < listaDeTeclas.length) {
+while (contador < listaDeTeclas.length) { */
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-                    //template string
-    const idAudio = `#som_${instrumento}`; 
+    const idAudio = `#som_${instrumento}`; //template string
 
     //console.log(idAudio);
-
-   tecla.onclick = function () {
+    tecla.onclick = function () {
         tocaSom(idAudio)
     }
 
-    contador = contador + 1;
-
     //console.log(contador);
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+
+        tecla.classList.remove('ativa');
+    }
 }
+
